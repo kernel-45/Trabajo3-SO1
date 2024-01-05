93646c41cf5670107e67d54d4191ae9ac6698765
Ignasi Paredes <133371990+ignasipc@users.noreply.github.com>
2024-01-05 18:55:47 +0100

    Update reader.c

--- reader.c
+++ reader.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "my_lib.h"
 #include "limits.h"
 
@@ -5,39 +6,37 @@
 #define ROJO "\x1b[31m"
 #define SUCCESS 0
 #define FAILURE -1
-#define N_ELEMENTS 10
+#define N_ELEMENTS 10.0
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     int min = INT_MAX; 
     int max = INT_MIN; 
     int sum = 0; 
-        if (argv[1] == NULL)
-    {
-        fprintf(stderr, ROJO, "Tienes que poner el nombre del fichero de la pila después del comando" RESET); 
+
+    if (argv[1] == NULL){
+        fprintf(stderr, ROJO "Tienes que poner el nombre del fichero de la pila después del comando\n" RESET); 
         return FAILURE; 
     }
 
-    struct my_stack *pila = my_stack_read(argv[1]); 
-    if (pila == NULL)
-    {
-        fprintf(stderr, ROJO "No se ha podido abrir el fichero o está vacío" RESET); 
+    struct my_stack *pila = my_stack_read(argv[1]);
+    
+    if (pila == NULL){
+        fprintf(stderr, ROJO "No se ha podido abrir el fichero o está vacío\n" RESET); 
     }
-    fprintf(stdout, "Elementos de la pila: \n"); 
+    fprintf(stdout, "Elementos de la pila: %d\n", my_stack_len(pila)); 
     for (size_t i = 0; i < N_ELEMENTS; i++)
     {
         int *data = my_stack_pop(pila); 
-        fprintf(stdout, "%d \n", data); 
-        if (data > max){
+        fprintf(stdout, "%ls \n", data); 
+        if (*data > max){
             max = *data; 
         }
-        if (data < min)
-        {
+        if (*data < min){
             min = *data; 
         }
         sum += *data; 
     }
-    fprintf(stdout, "El valor máximo es: %d\nEl valor mínimo es: %d\nLa suma total es: %d\nY la media es %f", max, min, sum, sum/N_ELEMENTS); 
+    fprintf(stdout, "El valor máximo es: %d\nEl valor mínimo es: %d\nLa suma total es: %d\nY la media es %f\n", max, min, sum, sum/N_ELEMENTS); 
     
     return 0; 
 }
